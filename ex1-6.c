ed5e4a8cae4022e7de1d7573e9fee672350a5c28
Subiksha MV <mvsubiksha@gmail.com>
2025-12-16 20:54:32 +0530

    Add output comments to ex1-6.c

--- ex1-6.c
+++ ex1-6.c
@@ -6,3 +6,19 @@ printf("\nEOF Result: 1\n");
 }
 printf("Reached EOF: %d\n",(c!=EOF));
 }
+
+/*output
+h
+Result: 1
+Result: 1
+b
+Result: 1
+Result: 1
+h
+Result: 1
+Result: 1
+u
+Result: 1
+Result: 1
+Reached EOF: 0
+*/
