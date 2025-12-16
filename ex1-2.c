d3c0be54cf9be2feaeed9467a202375f993873fe
Subiksha MV <mvsubiksha@gmail.com>
2025-12-16 20:47:33 +0530

    Document unknown escape sequences in ex1-2.c
    Added comments about unknown escape sequences in C.

--- ex1-2.c
+++ ex1-2.c
@@ -5,3 +5,8 @@ printf("Hello, World\t");
 printf("Hello, World\\");
 printf("Hello, \"World\"");
 }
+
+/* unknown escape sequence 
+\q - warning: unknown escape sequence \q
+\z - warning: unknown escape sequence \z
+*/
