6b3d3eaff5ba9824a773cf09ee583a7066c7cc9e
Subiksha MV <mvsubiksha@gmail.com>
2025-12-16 20:42:37 +0530

    Add comments explaining potential errors in ex1-1.c
    The code contains comments explaining errors that occur when removing various parts of the program.

--- ex1-1.c
+++ ex1-1.c
@@ -2,3 +2,21 @@
 main(){
 printf("Hello, World");
 }
+
+/*Leaving out parts of the program
+
+Remove #include <stdio.h>
+warning: implicit declaration of function ‘printf’
+
+Remove int main()
+error: expected declaration specifiers
+
+Remove semicolon ;
+error: expected ‘;’ before ‘return’
+
+Remove quotes " "
+error: ‘Hello’ undeclared
+
+Remove { }
+error: expected ‘{’ before ‘printf’
+*/
